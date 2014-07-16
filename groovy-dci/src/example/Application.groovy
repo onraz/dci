1072435fe8b89979123d3fd4e6e615e2030805a4
Raz <razibh@gmail.com>
2014-07-16 20:22:44 +1000

    typo

--- groovy-dci/src/example/Application.groovy
+++ groovy-dci/src/example/Application.groovy
@@ -7,14 +7,14 @@ import example.usecase.WithdrawalContext
 class Application {
 
 	static main(args) {
-def savings = new SavingsAccount(50.0)
-def checkings = new CheckingAccount(200.0)
-
-println "Before Tranfer: ${savings}, ${checkings}"
-
-new WithdrawalContext([source :checkings, dest: savings, amount:100]).execute()
-
-println "After Tranfer: ${savings}, ${checkings}"
+		def savings = new SavingsAccount(50.0)
+		def checkings = new CheckingAccount(200.0)
+		
+		println "Before Transfer: ${savings}, ${checkings}"
+		
+		new WithdrawalContext([source :checkings, dest: savings, amount:100]).execute()
+		
+		println "After Transfer: ${savings}, ${checkings}"
 	}
 
 }
